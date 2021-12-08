37eee6b5575a1a7ef0d708a8249ae6bea53989ec
Dmitriy Kryhtin <dmitriy.kryhtin@gmail.com>
2021-12-08 14:39:42 +0600

    Initial Commit

---
+++ HW_2.1_traffic_lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.1_traffic_lights
+//
+//  Created by Dmitriy Kryhtin on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
